1dcce68fe9002f6c5b0396f8ca4cd7eb17bce722
段昊辰 <duanhaochen@126.com>
2017-02-03 20:03:02 +0800

    Update start.js

--- start.js
+++ start.js
@@ -4,6 +4,6 @@ const options = require('./options.js');
 /* start */
 async function start(){
     const _1 = await buyTick(options['id'], options['brand_id'], options['seattype'], options['num']);
-    console.log(JSON.parse(_1[0], null, 4));
+    console.log(_1);
 }
 start();
