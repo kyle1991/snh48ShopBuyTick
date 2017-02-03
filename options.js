65a30713614a6f9d16ef0e94f747ae24b4e4edf4
段昊辰 <duanhaochen@126.com>
2017-02-03 19:03:57 +0800

    Create options.js

---
+++ options.js
@@ -0,0 +1,25 @@
+/**
+ * 配置
+ */
+
+module.exports = {
+    // 票务id
+    'id': 672,
+
+    // 票的数量
+    'num': 1,
+
+    // 票的类型。
+    // 2，VIP坐票；3，普通坐票；4，普通站票
+    'seattype': 4,
+
+    // 剧场类型。
+    // 1，SNH48；2，BEJ48；3，GNZ48；4，SHY48
+    'brand_id': 2,
+
+    // 抢票次数
+    'raceNumber': 1,
+
+    // cookie
+    'cookie': 'route=30fa7d7926dee7d6c6862a53da7c2b3b;.AspNet.ApplicationCookie=CWEJRV_p9q6p0w003RVDPB-iluqw0euxprv8Q4YZrgXr9uE9iJfpQTBGu-vn16QhCztEtRCcMT8TA2oOoI7KpJrfVPOXELHXvj12dmz3z9KzO5rIkJG4VrjcwT727dgIpm0mlWs58Ll-9kJYlRrNE_I1nsibO5ugP6hjztSxz3JyqGMUlXHwzTUPABaY9Dw9YIC0NHXXl47TkqJ_fAXePheo4AJwGbMseQMyYrU_AY3r7_BTbhc5xWJGJwrkPg8G3C17YVAAg1DNlmF8Zu6t1nrhJ8mtIIUbqeJaM7UzudXdsJBMfy5jZl0pzQz6KL3H9INs_wGPssU3skqaqeoGDl9LDqm6KODgbzKztijqqRAze-g394uEZzl9FIJtEjCSa-3A3ijvAKiTwKUhwtnLGhyD7JcDSB5Un5RIdV1e8gDOAsu-nU8w839fsHi29GPZ99EZrZkLqKBDH9hjZr4tnMoBo0GFiq4fY4GheryDE9TNLabNSsOmWW1L1waEnx-5FbjO-vJPcJ8VBZkfWA06Dc2_b064Js9Zxxu2Mj8n4UAIEwxpRbHVN1Y93lhWWPvfU04gY21NOR9xFLoYNnh3KpUpqr4DFrfgCQhfwP_jqKE;__RequestVerificationToken=09yc7_KNuOZNgpzYm8AWUau4q0mtsOHEyZqSk2QDpa0QefDT_6zx0mi0ARLf6r9vg5evHXydW9nbb1yo5tAvDwDvU-hXtIygZpua79MnpO01'
+};
