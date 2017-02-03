aa99380e573d2e221fa2088381dbd53111c49c9b
段昊辰 <duanhaochen@126.com>
2017-02-03 22:20:42 +0800

    Update buytick.js

--- kehuduan/buytick.js
+++ kehuduan/buytick.js
@@ -1,13 +1,13 @@
 /* 商城客户端抢票 */
-
 $(()=>{
-    const id = tickets_id;                            // 票务id
+    const id = tickets_id;                          // 票务id
     const seattype = $('#seat_type').val();         // 2：vip坐票，3：普通坐票，4：普通站票，可以自行修改成数字
-    const url = 'https://shop.48.cn/TOrder/add'; // 48商城地址
-    const num=$('#num').val();                       // 购票的数量，可以自行修改成数字
-    const brand_id = 2;                               // 1：SNH48，2：BEJ48，3：GNZ48，4：SHY48
-    const raceNumber = 1;                             // 抢票的次数
-    let set = null;                                   // 定时器
+    const url = 'https://shop.48.cn/TOrder/add';    // 48商城地址
+    const num=$('#num').val();                      // 购票的数量，可以自行修改成数字
+    const brand_id = 2;                             // 1：SNH48，2：BEJ48，3：GNZ48，4：SHY48
+    const raceNumber = 1;                           // 抢票的次数
+    const times = 2000;                             // ajax间隔时间（ms）
+    let set = null;                                 // 定时器
 
     function buyTick(){
         $.ajax({
