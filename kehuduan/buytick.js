5cf3ee9435b892b018e207b36d86463fd5657b8c
段昊辰 <duanhaochen@126.com>
2017-02-04 20:22:39 +0800

    Update buytick.js

--- kehuduan/buytick.js
+++ kehuduan/buytick.js
@@ -1,12 +1,11 @@
 /* 商城客户端抢票 */
 $(()=>{
-    const id = tickets_id;                          // 票务id
-    const seattype = $('#seat_type').val();         // 2：vip坐票，3：普通坐票，4：普通站票，可以自行修改成数字
+    const id = 668;                                 // 票务id
+    const seattype = 2;                             // 2：vip坐票，3：普通坐票，4：普通站票，可以自行修改成数字
     const url = 'https://shop.48.cn/TOrder/add';    // 48商城地址
-    const num=$('#num').val();                      // 购票的数量，可以自行修改成数字
+    const num = 1;                                  // 购票的数量，可以自行修改成数字
     const brand_id = 2;                             // 1：SNH48，2：BEJ48，3：GNZ48，4：SHY48
-    const raceNumber = 1;                           // 抢票的次数
-    const times = 2000;                             // ajax间隔时间（ms）
+    const times = 200;                              // ajax间隔时间（ms）
     let set = null;                                 // 定时器
 
     function buyTick(){
@@ -22,16 +21,16 @@ $(()=>{
                 brand_id: brand_id,
                 r: Math.random()
             },
-            success: (result)=>{
-                if(result.HasError){ // 没错误，表示抢票成功
-                    set = setTimeout(buyTick, 2000);
-                }else{
+            success: (result)=>{   // 有错误，继续抢
+                if(result.HasError){ 
+                    // set = setTimeout(buyTick, times);
+                }else{            // 没错误，表示抢票成功
                     console.log('抢票可能成功');
                     console.log(result);
-                    clearTimeout(set);
+                    clearInterval(set);
                 }
             }
         });
     }
-    buyTick();
+    set = setInterval(buyTick, times);
 });
